b3f3246524960332d5207f895706ba436cc6faf7
Mariana <mariana.fmedeiros@outlook.com>
2020-08-24 10:31:13 -0300

    add App and index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import cafecotech from './assets/cafecotech_lightblue.png';
 import './App.css';
+import TodoList from './components/TodoList';
+import { ListContextProvider } from './context/ListContext'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={cafecotech} className="App-logo" alt="logo" />
+        <ListContextProvider>
+          .: My Tasks :. 
+          <TodoList />
+        </ListContextProvider>
       </header>
     </div>
   );
